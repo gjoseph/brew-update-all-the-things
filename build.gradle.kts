2e2ae9bb06d7f531059019867a84f78e8c55c81b
Gregory Joseph <greg.jos@gmail.com>
2026-01-04 13:21:03 +1100

    Add macos universal binary

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ kotlin {
 
 group = "net.incongru.brewery"
 version = "1.0-SNAPSHOT"
-description = "An over-engineered script to routinely update a homebrew setup"
+description = "An over-engineered script to routinely update a Homebrew setup"
 
 repositories {
     mavenCentral()
@@ -24,6 +24,7 @@ kotlin {
             }
         }
     }
+
     listOf(
         macosArm64(), // Mac M1
         macosX64(),   // Mac Legacy
@@ -35,5 +36,25 @@ kotlin {
                 }
             }
         }
+    }
+}
+
+tasks.register("createUniversalBinary", Exec::class) {
+    // Only run on macOS, where 'lipo' is available.
+    onlyIf { System.getProperty("os.name").lowercase().contains("mac") }
+    val output = layout.buildDirectory.file("macos-universal/brew-update-all-the-things").get().asFile
+    output.parentFile.mkdirs()
+
+    commandLine(
+        "lipo",
+        "-create",
+        // TODO get those from above
+        "build/bin/macosArm64/releaseExecutable/brew-update-all-the-things.kexe",
+        "build/bin/macosX64/releaseExecutable/brew-update-all-the-things.kexe",
+        "-output", output
+    )
+
+    doLast {
+        println("Universal binary created at: $output")
     }
 }
